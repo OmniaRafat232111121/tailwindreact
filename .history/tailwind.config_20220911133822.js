2d5c767b6deeef9b9b98cc97d6f79c234fc1645a
omnia rafat <76200523+OmniaRafat232111121@users.noreply.github.com>
2022-09-11 14:56:55 +0200

    lists

---
+++ .history/tailwind.config_20220911133822.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
+    
+  },
+  plugins: [],
+};
---
+++ .history/tailwind.config_20220911133856.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+    extend: {
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
+      }
+    }
+  },
+  plugins: [],
+};
